b1355d8b2c2582986b61407d6b9548b2e0dacc88
panek <you@example.com>
2020-02-08 12:39:30 +0100

    step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,13 +2,13 @@
 import genDiff from '..';
 
 const program = require('commander');
- 
+
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format [type]  Output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action(function (firstConfig, secondConfig) {
+  .action((firstConfig, secondConfig) => {
     console.log(genDiff(firstConfig, secondConfig));
   });
 
