86263212a52f94957980b14f2445c36226442f08
panek <you@example.com>
2020-02-12 22:58:42 +0100

    step 6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,27 +2,42 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '..';
 
-
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+const cases = [
+  [getFixturePath('before.json'), getFixturePath('after.json'), readFile('result')],
+  [getFixturePath('before.yml'), getFixturePath('after.yml'), readFile('result')],
+  [getFixturePath('before.ini'), getFixturePath('after.ini'), readFile('result')],
+];
+
 describe('Comparing files tests', () => {
-  it('.json test', () => {
-    const before = getFixturePath('before.json');
-    const after = getFixturePath('after.json');
-    const result = readFile('result');
-    expect(genDiff(before, after)).toBe(result);
-  });
-  it('.yaml test', () => {
-    const before = getFixturePath('before.yml');
-    const after = getFixturePath('after.yml');
-    const result = readFile('result');
-    expect(genDiff(before, after)).toBe(result);
-  });
-  it('.ini test', () => {
-    const before = getFixturePath('before.ini');
-    const after = getFixturePath('after.ini');
-    const result = readFile('result');
-    expect(genDiff(before, after)).toBe(result);
-  });
+  test.each(cases)(
+    'test %s',
+    (before, after, result) => {
+      expect(genDiff(before, after)).toBe(result);
+    },
+  );
 });
+
+
+// describe('Comparing files tests', () => {
+//   it('.json test', () => {
+//     const before = getFixturePath('before.json');
+//     const after = getFixturePath('after.json');
+//     const result = readFile('result');
+//     expect(genDiff(before, after)).toBe(result);
+//   });
+//   it('.yaml test', () => {
+//     const before = getFixturePath('before.yml');
+//     const after = getFixturePath('after.yml');
+//     const result = readFile('result');
+//     expect(genDiff(before, after)).toBe(result);
+//   });
+//   it('.ini test', () => {
+//     const before = getFixturePath('before.ini');
+//     const after = getFixturePath('after.ini');
+//     const result = readFile('result');
+//     expect(genDiff(before, after)).toBe(result);
+//   });
+// });
