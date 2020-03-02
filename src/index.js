4e5d8fbfe07277b8d952186b4a8c18222a831a23
panek <you@example.com>
2020-03-02 12:18:20 +0100

    step 7

--- src/index.js
+++ src/index.js
@@ -7,41 +7,39 @@ const genDiff = (fileFirst, fileSecond) => {
   const objSecond = parser(fileSecond);
   const data = buildAst(objFirst, objSecond);
 
-  const renderObj = (obj, offset, space) => {
+  const renderObj = (obj, offset, tab) => {
     const keys = _.keys(obj);
-    return `{${keys.map((key) => `\n${offset}${space}${space}${key}: ${obj[key]}`)}\n${offset}}`;
+    return `{${keys.map((key) => `\n${offset}${tab}${tab}${key}: ${obj[key]}`)}\n${offset}}`;
   };
 
-  const renderValue = (value, offset, space) => {
-    return _.isObject(value) ? renderObj(value, (offset + space + space), space) : value;
-  };
+  const renderValue = (value, offset, tab) => (_.isObject(value) ? renderObj(value, (offset + tab + tab), tab) : value);
 
   const spaceCount = 2;
 
-  const iter = (data, offset = '', space = ' '.repeat(spaceCount)) => {
-    const result = data.reduce((acc, el) => {
+  const iter = (elements, offset = '', tab = ' '.repeat(spaceCount)) => {
+    const result = elements.reduce((acc, el) => {
       const {
         type, key, value, valueAfter, children,
       } = el;
 
       if (type === 'added') {
-        acc.push(`${offset}${space}+${space.slice(1)}${key}: ${renderValue(value, offset, space)}\n`);
+        acc.push(`${offset}${tab}+${tab.slice(1)}${key}: ${renderValue(value, offset, tab)}\n`);
       }
 
       if (type === 'removed') {
-        acc.push(`${offset}${space}-${space.slice(1)}${key}: ${renderValue(value, offset, space)}\n`);
+        acc.push(`${offset}${tab}-${tab.slice(1)}${key}: ${renderValue(value, offset, tab)}\n`);
       }
 
       if (type === 'nested') {
-        acc.push(`${offset}${space}${space}${key}: {\n${iter(children, (offset + space + space), space)}${offset}${space}${space}}\n`);
+        acc.push(`${offset}${tab}${tab}${key}: {\n${iter(children, (offset + tab + tab), tab)}${offset}${tab}${tab}}\n`);
       }
       if (type === 'equal') {
-        acc.push(`${offset}${space}${space}${key}: ${renderValue(value, offset, space)}\n`);
+        acc.push(`${offset}${tab}${tab}${key}: ${renderValue(value, offset, tab)}\n`);
       }
 
       if (type === 'modified') {
-        acc.push(`${offset}${space}+${space.slice(1)}${key}: ${renderValue(valueAfter, offset, space)}\n`);
-        acc.push(`${offset}${space}-${space.slice(1)}${key}: ${renderValue(value, offset, space)}\n`);
+        acc.push(`${offset}${tab}+${tab.slice(1)}${key}: ${renderValue(valueAfter, offset, tab)}\n`);
+        acc.push(`${offset}${tab}-${tab.slice(1)}${key}: ${renderValue(value, offset, tab)}\n`);
       }
 
       return acc;
