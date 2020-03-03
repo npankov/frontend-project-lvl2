0a2d962ad7f678aebf5e4fea2059f18e7f7694d8
panek <you@example.com>
2020-03-03 15:41:14 +0100

    step 8

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,41 +6,14 @@ const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filena
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = [
-  [getFixturePath('before.json'), getFixturePath('after.json'), readFile('result')],
-  [getFixturePath('before.yml'), getFixturePath('after.yml'), readFile('result')],
-  [getFixturePath('before.ini'), getFixturePath('after.ini'), readFile('result')],
-  [getFixturePath('beforeNested.json'), getFixturePath('afterNested.json'), readFile('resultNested')],
-  [getFixturePath('beforeNested.yml'), getFixturePath('afterNested.yml'), readFile('resultNested')],
-  [getFixturePath('beforeNested.ini'), getFixturePath('afterNested.ini'), readFile('resultNested')],
+  [getFixturePath('beforeNested.json'), getFixturePath('afterNested.json'), 'nested', readFile('resultNested')],
+  [getFixturePath('beforeNested.yml'), getFixturePath('afterNested.yml'), 'plain', readFile('resultPlain')],
+  [getFixturePath('beforeNested.ini'), getFixturePath('afterNested.ini'), 'nested', readFile('resultNested')],
+  [getFixturePath('beforeNested.ini'), getFixturePath('afterNested.ini'), 'plain', readFile('resultPlain')],
 ];
 
 describe('Comparing files tests', () => {
-  test.each(cases)(
-    ('test %s'),
-    (before, after, result) => {
-      expect(genDiff(before, after)).toBe(result);
-    },
-  );
+  test.each(cases)('test %#', (before, after, format, result) => {
+    expect(genDiff(before, after, format)).toBe(result);
+  });
 });
-
-
-// describe('Comparing files tests', () => {
-//   it('.json test', () => {
-//     const before = getFixturePath('before.json');
-//     const after = getFixturePath('after.json');
-//     const result = readFile('result');
-//     expect(genDiff(before, after)).toBe(result);
-//   });
-//   it('.yaml test', () => {
-//     const before = getFixturePath('before.yml');
-//     const after = getFixturePath('after.yml');
-//     const result = readFile('result');
-//     expect(genDiff(before, after)).toBe(result);
-//   });
-//   it('.ini test', () => {
-//     const before = getFixturePath('before.ini');
-//     const after = getFixturePath('after.ini');
-//     const result = readFile('result');
-//     expect(genDiff(before, after)).toBe(result);
-//   });
-// });
